c6eeae5cab963149b1925c97d1b8759ab6720d7a
vanishap <vanishareddyp@gmail.com>
2023-05-18 16:24:24 -0700

    list added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ function App() {
   const [count, setCount] = useState(0);
   const [todos, setTodos]= useState(['Buy Groceries', 'School pick up']);
   console.log('rendered from app');
+
+  const clickHandler = ()=>{
+    setCount(count=>count +1);
+  }
+  const todoHandler=()=>{
+    setTodos((todos)=>[...todos, 'laundry','do dishes']);
+  }
   return (
     <div className="App">
       <Todos todos= {todos}/>
       <div>
         Count: {count}
-        <button onClick = {()=>setCount(count =>count + 1)}>+</button>
+        <button onClick = {clickHandler}>+</button>
+        <button onClick={todoHandler}>Add todo</button>
       </div>
       
     </div>
